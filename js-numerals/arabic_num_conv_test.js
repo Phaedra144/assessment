a2cfe305ce7534d753e963947875e38d4d35d73c
Phaedra144 <bedi.szilvia@gmail.com>
2018-10-11 19:43:18 +0200

    feature(input-field) failing test

--- js-numerals/arabic_num_conv_test.js
+++ js-numerals/arabic_num_conv_test.js
@@ -1,7 +1,12 @@
 'use strict';
 const test = require('tape');
 
-test('Submit button exists and ok', function (t) {
-  t.ok(submitButt);
+test('Input field exists and not ok', function (t) {
+  t.ok(input_number);
+  t.end();
+});
+
+test('Submit button exists and not ok', function (t) {
+  t.skip(submitButt);
   t.end();
 });
