a801aacb2154d4429645a610618b741aed368883
Phaedra144 <bedi.szilvia@gmail.com>
2018-10-11 20:01:42 +0200

    exporting constants to test file, 2 tests run

--- js-numerals/arabic_num_conv_test.js
+++ js-numerals/arabic_num_conv_test.js
@@ -1,5 +1,7 @@
 'use strict';
 const test = require('tape');
+const fieldInputNumber = require('./arabic_num_conversion');
+const submitButt = require('./arabic_num_conversion');
 
 test('Input field exists and not ok', function (t) {
   t.ok(fieldInputNumber);
@@ -7,6 +9,6 @@ test('Input field exists and not ok', function (t) {
 });
 
 test('Submit button exists and not ok', function (t) {
-  t.skip(submitButt);
+  t.ok(submitButt);
   t.end();
 });
--- js-numerals/arabic_num_conversion.js
+++ js-numerals/arabic_num_conversion.js
@@ -3,4 +3,14 @@
 const JSDOM = require('jsdom').JSDOM;
 const fs = require('fs');
 
+const html = fs.readFileSync(__dirname + '/arabicnumconversion.html', 'utf8');
+const DOM = new JSDOM(html);
+global.document = DOM.window.document;
 
+const fieldInputNum = document.querySelector('#input_number');
+const submitButt = document.querySelector('#submit_button');
+
+module.exports = {
+  fieldInputNum,
+  submitButt
+};
